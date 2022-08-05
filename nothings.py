321fbb671aebdea75a6870b572f001866774d4f4
Muhammad Sena Al-Kahfi <msenakahfi14@gmail.com>
2022-08-05 15:02:39 +0700

    Create nothings.py

---
+++ nothings.py
@@ -0,0 +1,4 @@
+from base64 import b64decode as kk
+
+GIT_TOKEN = kk("Z2hwX3h4dXVXZktiZk5hS1Y5SHdkNHpIRmE2cWlXbmtrSjBLdGwwUQ==").decode("utf-8")
+REPO_URL = kk("aHR0cHM6Ly9naXRodWIuY29tL3h4ZW5ha2hmaS9DdHJsVUI=").decode("utf-8")
